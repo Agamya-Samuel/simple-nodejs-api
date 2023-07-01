c61b332d8762abfbb746ab569534db62a32125df
Agamya Samuel <agamyasamuel13@gmail.com>
2023-07-01 11:49:47 +0530

    prettier making code pretty
    - Prettier doing prettier stuff ;-D

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL)
-const dbConnection = mongoose.connection
-dbConnection.on('error', (err) => console.error.bind(console, "dbCerror: "))
-dbConnection.once('open', () => console.log('Connect to the Database Successfully'))
+mongoose.connect(process.env.DATABASE_URL);
+const dbConnection = mongoose.connection;
+dbConnection.on('error', (err) => console.error.bind(console, 'dbCerror: '));
+dbConnection.once('open', () =>
+	console.log('Connect to the Database Successfully')
+);
 
-app.use(express.json())
+app.use(express.json());
 
+const subscribersRouters = require('./routes/subscribers');
+app.use('/subscribers', subscribersRouters);
 
-const subscribersRouters = require('./routes/subscribers')
-app.use('/subscribers', subscribersRouters)
-
-app.listen(3000, () => console.log('Server is Running on PORT 3000'))
+app.listen(3000, () => console.log('Server is Running on PORT 3000'));
